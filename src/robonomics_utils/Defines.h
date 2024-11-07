ff993c5df8e4a8a1ac2f702fbab279e5d4e5b530
LoSk-p <alki.spb@mail.ru>
2024-11-07 09:07:59 +0300

    change endpoint to polkadot

--- src/robonomics_utils/Defines.h
+++ src/robonomics_utils/Defines.h
@@ -3,7 +3,8 @@
 #define KEYS_SIZE 32
 #define SS58_ADDRESS_SIZE 48
 
-#define WS_SERVER_HOST "kusama.rpc.robonomics.network"
+// #define WS_SERVER_HOST "kusama.rpc.robonomics.network"
+#define WS_SERVER_HOST "polkadot.rpc.robonomics.network"
 #define WS_SERVER_PORT 443
 
 #define SIGNATURE_SIZE 64
