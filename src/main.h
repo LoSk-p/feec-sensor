1f3ecdde65c1a32881436ed37988ddb5a21d973f
LoSk-p <alki.spb@mail.ru>
2024-11-08 19:37:12 +0300

    update timeouts

--- src/main.h
+++ src/main.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define SERVER_SENDING_TIMEOUT 60000 // ms
-#define DATALOG_SENDING_TIMEOUT 180000 // ms
+#define DATALOG_SENDING_TIMEOUT 600000 // ms
 #define WIFI_CONNECTING_DELAY 10000 //ms
 
 #define DHT_USE // Use DHT22 sensor
